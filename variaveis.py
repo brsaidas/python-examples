7d872df1d8e3bc2a4c653e8e415cdaef16112b20
Bruno Said Alves <86388642+brsaidas@users.noreply.github.com>
2022-11-28 20:53:34 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,11 @@
+# Exemplo de aplicação de cadastro de usuário
+
+usuario = input("Entre com o usuário: ")
+senha = input("Entre com a senha: ")
+
+"""
+Depois da entrada dos dados podedemos
+executar o processamento.
+"""
+
+print("Usuario:", usuario, "com Senha:", senha, "cadastrado com sucesso")
