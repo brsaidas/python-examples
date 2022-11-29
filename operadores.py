42881f6922d71234049236d630e67e76b0e8fbf2
Bruno Said Alves <86388642+brsaidas@users.noreply.github.com>
2022-11-28 21:15:13 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,26 @@
+# Definicao de algumas variáveis para teste
+altura1 = 10.56
+altura2 = 7.80
+idade1 = 10
+idade2 = 20
+nome1 = "vaca"
+nome2 = "cavalo"
+
+# Operadores condicionais
+print("operadoes condicionais")
+
+print(altura1 > altura2)
+print(idade1 <= idade2)
+print(nome1 != nome2)
+print(altura1 == idade1)
+
+# Operadores matemáticos
+print("Operadores matemáticos")
+
+print(altura1 + altura2)
+print(idade1 - idade2)
+print(idade1 * idade2)
+print(idade1 / idade2)
+print(idade1 // idade2)
+print(idade1 % idade2)
+print(idade1 ** idade2)
